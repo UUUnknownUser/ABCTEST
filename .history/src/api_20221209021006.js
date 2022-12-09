36616e02ea6705e53945068100efb159abdc8bb1
UUUnknownUser <113260742+UUUnknownUser@users.noreply.github.com>
2022-12-09 02:10:22 -0500

    test

--- .history/src/api_20221209021006.js
+++ .history/src/api_20221209021006.js
@@ -9,11 +9,11 @@ const {surveyList}=require('../db/models/surveyList.server.model');
 
 router.get('/json',(req,res)=>{
 
-    res.json({
+    // res.json({
 
-        'hello':'hi!'
+    //     'hello':'hi!'
 
-    });
+    // });
    
         //return an array of the survey
         // surveyList.find({}).then((surveyList)=>{
